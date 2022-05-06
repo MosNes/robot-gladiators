9075e5f68caa16bc7b805f9d49c5c7d9417a7ec0
MosNes <7hekarl@gmail.com>
2022-05-06 16:56:03 -0400

    added enemy names array

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,9 @@
+// Game States
+// WIN - Player robot has defeated all enemy robots
+//  * Fight all enemy-robots
+//  * Defeat each enemy-robot
+// LOSE - Player robot's health is zero or less
+
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
@@ -5,7 +11,8 @@ var playerMoney = 10;
 
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto","Amy Android","Robo Trumble"];
+console.log("Enemies: "+enemyNames);
 var enemyHealth = 50;
 var enemyAttack = 12;
 
@@ -57,4 +64,4 @@ var fight = function () {
     }
 };
 
-fight();
+// fight();
