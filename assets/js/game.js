e64c80f1ae6e5b286610ee304edd22448d69d6aa
MosNes <7hekarl@gmail.com>
2022-05-07 18:33:13 -0400

    added for loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,11 +12,10 @@ var playerMoney = 10;
 console.log(playerName, playerAttack, playerHealth);
 
 var enemyNames = ["Roborto","Amy Android","Robo Trumble"];
-console.log("Enemies: "+enemyNames);
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function () {
+var fight = function (enemyName) {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -64,4 +63,6 @@ var fight = function () {
     }
 };
 
-// fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
